a3b049dbbfb3addc0d4cff24796f9612accc1ef7
c4181 <cm4181@gmail.com>
2019-11-16 02:19:41 -0500

    Brute force can add numbers as long as the partial products don't need a carry

--- main.cpp
+++ main.cpp
@@ -16,6 +16,106 @@ void padLeft(vector<int> &the_vector, size_t number_of_zeroes) {
   }
 }
 
+void carryDigits(vector<int> &digits, int num) {
+  if (num > 9) {
+    carryDigits(digits, num / 10);
+  }
+  digits.push_back(num % 10);
+}
+
+void addPartialProducts(vector<int> a, vector<int> &c) {
+  int carry = 0;
+  vector<int> carry_vector;
+  padLeft(a, c.size() - a.size());
+
+  for (size_t i = c.size(); i > 0; --i) {
+    int temp = a.at(i - 1) + c.at(i - 1);
+
+    // Check for numbers to be carried
+    if (carry > 0) {
+      temp += carry;
+      carry = 0;
+      carry_vector.clear();
+    }
+    if (temp > 9) {
+      carryDigits(carry_vector, temp);
+      carry = carry_vector.at(0);
+      c.at(i - 1) += carry_vector.at(1);
+    } else {
+      c.at(i - 1) += temp;
+    }
+  }
+  if (carry > 0) {
+    c.insert(c.begin(), carry);
+  } 
+}
+
+// TODO Helper function to add partial products
+// This function can't handle carrying in the addition
+vector<int> bruteForceMultiply(vector<int> a, vector<int> b) {
+  int carry = 0;
+  vector<int> carry_vector;
+  vector<int> c;
+
+  // Initalize c with all 0's
+  if (a.size() < b.size()) {
+    for (size_t i = 0; i < b.size(); ++i) {
+      c.push_back(0);
+    }
+  } else {
+    for (size_t i = 0; i < a.size(); ++i) {
+      c.push_back(0);
+    }
+  }
+
+  int count = 0;
+  for (int j = b.size() - 1 ; j >= 0; --j) {
+    cout << "***********OUTER LOOP **************" << endl;
+    carry_vector.clear();
+    vector<int>::reverse_iterator it = c.rbegin() + count;
+    for (int i = a.size() - 1; i >= 0; --i) {
+      cout << "Multiply " << a.at(i) << " * " << b.at(j) << endl;
+      int temp = a.at(i) * b.at(j);
+      cout << "Answer: " << temp << endl;
+      // Check for numbers to be carried
+      if (carry > 0) {
+        temp += carry;
+        cout << "Adding the carry: " << carry << endl;
+        carry = 0;
+        carry_vector.clear();
+      }
+      if (temp > 9) {
+        carryDigits(carry_vector, temp);
+        carry = carry_vector.at(0);
+        cout << "Carrying a " << carry << endl;
+        cout << "Adding " << carry_vector.at(1) << " to " << *it << endl;
+        *it += carry_vector.at(1);
+        cout << "Partial Product: " << *it << endl;
+      } else {
+        *it += temp;
+        cout << "Partial Product: " << *it << endl;
+      }
+      ++it;
+      if (it == c.rend()) {
+        if (carry > 0) {
+          c.insert(c.begin(), carry);
+          carry = 0;
+        }
+      }
+      // DEBUG
+      cout << "C looks like: ";
+      for (size_t i = 0; i < c.size(); ++i) {
+        cout << c.at(i) << " ";
+      }
+      cout << endl;
+      cout << "i is " << i << endl;
+      cout << "j is " << j << endl;
+    }
+    ++count;
+  }
+  return c;
+}
+
 int main() {
   vector<int> a;
   vector<int> b;
@@ -46,5 +146,11 @@ int main() {
       size_t number_of_zeroes = a.size() - b.size();
       padLeft(b, number_of_zeroes);
     }
+  }
+
+  c = bruteForceMultiply(a, b);
+
+  for (size_t i = 0; i < c.size(); ++i) {
+    cout << c.at(i) << " ";
   }
 }
